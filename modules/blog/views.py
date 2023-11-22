4c0043646dccd323d589903c25dbae0ad523284e
Teonad <svistun.sasha2013@yandex.ru>
2023-11-22 16:38:49 +0300

    add

--- modules/blog/views.py
+++ modules/blog/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import ListView, DetailView
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from ..services.mixins import AuthorRequiredMixin
+from django.urls import reverse_lazy
 from .models import Article, Category
+from .forms import ArticleCreateForm, ArticleUpdateForm
 
 
 class ArticleListView(ListView):
@@ -42,3 +46,58 @@ class ArticleByCategoryListView(ListView):
         context['title'] = f'Статьи из категории: {self.category.title}'
         return context
 
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    """
+    Представление: создание материалов на сайте
+    """
+    model = Article
+    template_name = 'blog/articles_create.html'
+    form_class = ArticleCreateForm
+    login_url = 'home'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Добавление статьи на сайт'
+        return context
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.save()
+        return super().form_valid(form)
+
+
+class ArticleUpdateView(AuthorRequiredMixin, SuccessMessageMixin, UpdateView):
+    """
+    Представление: обновления материала на сайте
+    """
+    model = Article
+    template_name = 'blog/articles_update.html'
+    context_object_name = 'article'
+    form_class = ArticleUpdateForm
+    login_url = 'home'
+    success_message = 'Материал был успешно обновлен'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Обновление статьи: {self.object.title}'
+        return context
+
+    def form_valid(self, form):
+        # form.instance.updater = self.request.user
+        form.save()
+        return super().form_valid(form)
+
+
+class ArticleDeleteView(AuthorRequiredMixin, DeleteView):
+    """
+    Представление: удаления материала
+    """
+    model = Article
+    success_url = reverse_lazy('home')
+    context_object_name = 'article'
+    template_name = 'blog/articles_delete.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Удаление статьи: {self.object.title}'
+        return context
