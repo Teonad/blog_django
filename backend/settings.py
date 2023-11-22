0d49d9b04184568521135c21c1ff237a21e0115a
Teonad <svistun.sasha2013@yandex.ru>
2023-11-22 15:07:37 +0300

    add

--- backend/settings.py
+++ backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'modules.blog.apps.BlogConfig',
     'modules.services',
     'mptt',
+    'debug_toolbar',
 
 ]
 
@@ -52,6 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'backend.urls'
