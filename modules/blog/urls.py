c994f757989e14634cfcfb17dd2243be56b01c11
Teonad <svistun.sasha2013@yandex.ru>
2023-11-27 14:54:39 +0300

    add

--- modules/blog/urls.py
+++ modules/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ArticleListView, ArticleDetailView, ArticleByCategoryListView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView
+from .views import ArticleListView, ArticleDetailView, ArticleByCategoryListView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView, CommentCreateView
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='home'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('category/<str:slug>/', ArticleByCategoryListView.as_view(), name="articles_by_category"),
     path('articles/<str:slug>/update/', ArticleUpdateView.as_view(), name='articles_update'),
     path('articles/<str:slug>/delete/', ArticleDeleteView.as_view(), name='articles_delete'),
+    path('articles/<int:pk>/comments/create/', CommentCreateView.as_view(), name='comment_create_view'),
 ]
